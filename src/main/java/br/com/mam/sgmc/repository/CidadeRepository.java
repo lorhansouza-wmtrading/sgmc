251038c123bad28f26ae316356e3530148fead30
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-14 13:30:32 -0300

    :sparkles: feat: adicionar query com filtro nos repositórios

--- src/main/java/br/com/mam/sgmc/repository/CidadeRepository.java
+++ src/main/java/br/com/mam/sgmc/repository/CidadeRepository.java
@@ -1,10 +1,26 @@
 package br.com.mam.sgmc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.mam.sgmc.model.localizacao.Cidade;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
     Cidade findByNome(String nome);
+
+    @Query("SELECT c FROM Cidade c" + 
+            " JOIN  c.uf u" + 
+            " JOIN  u.pais p" + 
+            " WHERE " + 
+            "(:ufSigla IS NULL OR u.ufSigla = :ufSigla) AND " +
+            "(:nome IS NULL OR c.nome = :nome) AND " +
+            "(:pais IS NULL OR p.nome = :pais)")
+    List<Cidade> findWithFilters(
+        String ufSigla,
+        String nome,
+        String pais
+    );
 
 }
--- src/main/java/br/com/mam/sgmc/repository/PaisRepository.java
+++ src/main/java/br/com/mam/sgmc/repository/PaisRepository.java
@@ -1,6 +1,9 @@
 package br.com.mam.sgmc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.mam.sgmc.model.localizacao.Pais;
 
@@ -9,5 +12,15 @@ public interface PaisRepository extends JpaRepository<Pais, String> {
     Pais findByNome(String nome);
 
     Pais findBySigla(String sigla);
+
+    @Query("SELECT p FROM Pais p WHERE " +
+            "(:sigla IS NULL OR p.sigla = :sigla) AND " +
+            "(:nome IS NULL OR p.nome = :nome) AND " +
+            "(:continente IS NULL OR p.continente = :continente)")
+    List<Pais> findWithFilters(
+        String sigla, 
+        String nome, 
+        String continente
+    );
 
 }
