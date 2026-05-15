a18dadecf20765954f69e3a094e697188db235db
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-15 14:56:52 -0300

    :sparkles: feat: adicionar `ResourceNotFoundException` ao serviço

--- src/main/java/br/com/mam/sgmc/services/LocalizacaoService.java
+++ src/main/java/br/com/mam/sgmc/services/LocalizacaoService.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import br.com.mam.sgmc.api.dto.response.CidadeResponseDTO;
-import br.com.mam.sgmc.api.dto.response.PaisResponseDTO;
-import br.com.mam.sgmc.api.dto.response.UfResponseDTO;
+import br.com.mam.sgmc.errors.ResourceNotFoundException;
 import br.com.mam.sgmc.model.localizacao.Cidade;
 import br.com.mam.sgmc.model.localizacao.Pais;
 import br.com.mam.sgmc.model.localizacao.Uf;
@@ -24,15 +22,27 @@ public class LocalizacaoService {
     private final CidadeRepository cidadeRepository;
 
     public List<Pais> listarPaisesComFiltros(String sigla, String nome, String continente) {
-        return paisRepository.findWithFilters(sigla, nome, continente);
+        List<Pais> paises = paisRepository.findWithFilters(sigla, nome, continente);
+        if (paises.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi possível encontrar nenhum país com os filtros informados");
+        }
+        return paises;
     }
 
     public List<Uf> listarUfsComFiltros(String sigla, String nome, String regiao, String pais) {
-        return ufRepository.findWithFilters(sigla, nome, regiao, pais);
+        List<Uf> ufs = ufRepository.findWithFilters(sigla, nome, regiao, pais);
+        if (ufs.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi possível encontrar nenhuma UF com os filtros informados");
+        }
+        return ufs;
     }
 
     public List<Cidade> listarCidadesComFiltros(String ufSigla, String nome, String pais) {
-        return cidadeRepository.findWithFilters(ufSigla, nome, pais);
+        List<Cidade> cidades = cidadeRepository.findWithFilters(ufSigla, nome, pais);
+        if (cidades.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi possível encontrar nenhuma cidade com os filtros informados");
+        }
+        return cidades;
     }
 
 }
