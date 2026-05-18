9dadd4db3f62c1698235ccc15d0253f4f73325b5
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-18 13:19:30 -0300

    :books: docs: alterar saída do método de Void para String

--- src/main/java/br/com/mam/sgmc/api/openapi/MembroControllerOpenAPI.java
+++ src/main/java/br/com/mam/sgmc/api/openapi/MembroControllerOpenAPI.java
@@ -44,7 +44,7 @@ public interface MembroControllerOpenAPI {
             @ApiResponse(responseCode = "201", description = "Membro criado com sucesso"),
             @ApiResponse(responseCode = "400", description = "Requisição inválida")
     })
-    ResponseEntity<Void> criarMembro(@RequestBody @Valid MembroRequestDTO membroDTO);
+    ResponseEntity<String> criarMembro(@RequestBody @Valid MembroRequestDTO membroDTO);
 
     @Operation(summary = "Inativa um membro", description = "Desativa um membro, marcando-o como inativo.")
     @ApiResponses(value = {
