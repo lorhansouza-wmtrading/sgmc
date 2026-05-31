5f1796cb2dce8cf29e05f4a796b1385a012cca4d
Lorhan Souza <lorhan.s160@gmail.com>
2026-05-31 17:23:11 -0300

    :sparkles: feat: adicionar filtro de busca no repository usando JPQL

--- src/main/java/br/com/mam/sgmc/repository/MotoRepository.java
+++ src/main/java/br/com/mam/sgmc/repository/MotoRepository.java
@@ -1,9 +1,21 @@
 package br.com.mam.sgmc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.mam.sgmc.model.moto.Moto;
 
 public interface MotoRepository extends JpaRepository<Moto, String> {
     Moto findByPlaca(String placa);
+
+    void deleteByPlaca(String placa);
+
+    @Query("SELECT m FROM Moto m " +
+            "WHERE (:idMembro IS NULL OR m.membro.id = :idMembro) " +
+            "AND (:modelo IS NULL OR m.modelo.nome = :modelo) " +
+            "AND (:marca IS NULL OR m.modelo.marca.nome = :marca) " +
+            "AND (:seguro IS NULL OR m.seguro.nome = :seguro)")
+    List<Moto> findWithFilters(Long idMembro, String modelo, String marca, String seguro);
 }
