a00aca7ed94aa9a7fbe227ea83e241847a61cefc
Lorhan Souza <lorhan.s160@gmail.com>
2026-06-02 21:54:25 -0300

    :wrench: chore: definir mapeamento por `ROLE` (papel-cargo)

--- src/main/java/br/com/mam/sgmc/config/SecurityConfig.java
+++ src/main/java/br/com/mam/sgmc/config/SecurityConfig.java
@@ -5,18 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/v3/**").permitAll()
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                 .requestMatchers("/api/**").authenticated()
-                .anyRequest().permitAll());
+                .anyRequest().permitAll())
+            .oauth2ResourceServer(oauth -> oauth
+                .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(jwtAuthorityConverter())));
         return http.build();
     }
+
+    private JwtAuthenticationConverter jwtAuthorityConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthorityPrefix("ROLE_");
+        converter.setAuthoritiesClaimName("roles");
+        JwtAuthenticationConverter authConverter = new JwtAuthenticationConverter();
+        authConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authConverter;
+    }
 }
+
